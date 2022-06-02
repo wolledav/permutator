e1029a8f87298ed12b3442586affe0ea5da3a04b
David Woller <wolledav@fel.cvut.cz>
2022-06-02 16:18:26 +0200

    cmdline arguments to json config file added

---
+++ tools/create_config.py
@@ -0,0 +1,41 @@
+import sys
+import json
+
+output_file = sys.argv[1]
+params = sys.argv[2:]
+operators = []
+perturbations = []
+config = {}
+
+for i in range(0, len(params), 2):
+    param = params[i]
+    value = params[i + 1]
+
+    if param[0:3] == "op_":
+        if value == '1':
+            operators.append(param[3:])
+    elif param == "perturbation":
+        perturbations.append(value)
+    elif param == "allow_infeasible":
+        config[param] = bool(value)
+    elif param == "ils_k":
+        config[param] = int(value)
+    elif param == "construction":
+        config[param] = value
+    elif param == "metaheuristic":
+        config[param] = value
+    elif param == "local_search":
+        config[param] = value
+    elif param == "timeout":
+        config[param] = int(value)
+    elif param == "num_threads":
+        config[param] = int(value)
+    else:
+        print(sys.argv[0], "unknown parameter:", param)
+
+config["operators"] = operators
+config["perturbation"] = perturbations
+
+file = open(output_file, "w")
+json.dump(config, file, indent=4)
+file.close()
