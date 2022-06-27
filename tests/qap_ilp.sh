7894bc7072be9950ce28a1b0adb6571cf737302c
David Woller <wolledav@fel.cvut.cz>
2022-06-27 14:20:48 +0200

    test script updated

--- tests/qap_ilp.sh
+++ tests/qap_ilp.sh
@@ -7,7 +7,9 @@ LOGDIR="../log/qap-ilp/$TIMESTAMP"
 DATASETS=( "../data/qap-taillard/tai20b.dat" "../data/qap-taillard/tai25a.dat" "../data/qap-taillard/tai30b.dat"
           "../data/qap-taillard/tai40a.dat" "../data/qap-taillard/tai50b.dat" "../data/qap-taillard/tai60a.dat"
           "../data/qap-taillard/tai80a.dat" "../data/qap-taillard/tai80b.dat" "../data/qap-taillard/tai100a.dat" "../data/qap-taillard/tai100b.dat" )
-TIMEOUTS=( 600 600 600 1800 1800 1800 3600 3600 3600 3600 )
+# TIMEOUTS=( 600 600 600 1800 1800 1800 3600 3600 3600 3600 )             # default time
+TIMEOUTS=( 6000 6000 6000 18000 18000 18000 36000 36000 36000 36000 )   # default time x 10
+
 
 echo $LOGDIR
 # Create log directory
