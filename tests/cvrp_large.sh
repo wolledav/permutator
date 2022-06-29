a7a74d624fcee8afa982d91647d7c78467536091
David Woller <wolledav@fel.cvut.cz>
2022-06-29 12:10:13 +0200

    cvrp meta test scripts updated

--- tests/cvrp_large.sh
+++ tests/cvrp_large.sh
@@ -4,8 +4,9 @@ METAOPT_BIN="../cmake-build-release"
 TEST_TIMEOUT=3600 # 60 minutes
 TIMESTAMP=$(date +'%Y%m%d-%H%M%S')
 LOGDIR="../log/cvrp-large/$TIMESTAMP"
+CONFIG="../configs/cvrp_final_2.json"
 
-DATASETS=( "../data/cvrp/cvrp-uchoa/X-n351-k40.xml" "../data/cvrp/cvrp-uchoa/X-n561-k42.xml" "../data/cvrp/cvrp-uchoa/X-n749-k98.xml" "../data/cvrp/cvrp-uchoa/X-n1001-k43.xml" )
+DATASETS=( "../data/cvrp/cvrp-10/X-n351-k40.xml" "../data/cvrp/cvrp-10/X-n561-k42.xml" "../data/cvrp/cvrp-10/X-n749-k98.xml" "../data/cvrp/cvrp-10/X-n1001-k43.xml" )
 
 echo $LOGDIR
 # Create log directory
@@ -14,10 +15,11 @@ cmake -E make_directory "$LOGDIR"
 for data in "${DATASETS[@]}"
 do
   filename=$(basename $data ".xml")
-  for i in {1..50}
+  for i in {1..5}
   do
     echo "[$i/50] Testing ${filename}..."
-    $METAOPT_BIN/cvrp_meta -d $data -t $TEST_TIMEOUT -o "${LOGDIR}/$i-meta-${filename}.out" > /dev/null 2>&1
+    cmake -E make_directory "$LOGDIR"/"$filename"
+    $METAOPT_BIN/cvrp_meta -d $data -c $CONFIG -t $TEST_TIMEOUT -o "${LOGDIR}/${filename}/$i-meta-${filename}.out"
   done
-  $METAOPT_BIN/cvrp_ilp -d $data -t $TEST_TIMEOUT -o "${LOGDIR}/ilp-${filename}.out" > /dev/null 2>&1
+#  $METAOPT_BIN/cvrp_ilp -d $data -t $TEST_TIMEOUT -o "${LOGDIR}/ilp-${filename}.out" > /dev/null 2>&1
 done
