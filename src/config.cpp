de4e8c4b645c85e5840d11450e0bb18263547d2a
David Woller <wolledav@fel.cvut.cz>
2022-09-02 10:03:40 +0200

    default config path changed

--- src/config.cpp
+++ src/config.cpp
@@ -1,5 +1,5 @@
 #include "config.hpp"
 
 json Config::read_default_config(const string& problem_type) {
-    return read_json("../configs/default_config.json")[problem_type];
+    return read_json("./configs/default_config.json")[problem_type];
 }
