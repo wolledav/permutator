52be910cdb3c62588972377fd9cfa18435984db3
David Woller <wolledav@fel.cvut.cz>
2022-09-02 10:08:07 +0200

    drawing scipts updated

--- tools/tspsd/draw_solutions.sh
+++ tools/tspsd/draw_solutions.sh
@@ -2,14 +2,14 @@
 # run from permutator/
 
 CMD="python3 ./tools/tspsd/draw_wcp.py"
+INSTANCES=("./data/tspsd/berlin52_random/")
+SOLUTIONS="./log/wcp-meta/berlin52_random_10min/"
+OUTDIR="./figures/wcp/berlin52_random_10min_solved/"
 
+#CMD="python3 ./tools/tspsd/draw_scp.py"
 #INSTANCES=("./data/tspsd/berlin52_random/")
-#SOLUTIONS="./log/wcp-meta/berlin52_random/"
-#OUTDIR="./figures/wcp/berlin52_random_solved/"
-
-INSTANCES=("./data/tspsd/berlin52_closest/")
-SOLUTIONS="./log/wcp-meta/berlin52_closest_10min/"
-OUTDIR="./figures/wcp/berlin52_closest_10min_solved/"
+#SOLUTIONS="./log/scp-meta/berlin52_random_10min/"
+#OUTDIR="./figures/scp/berlin52_random_10min_solved/"
 
 cmake -E make_directory "$OUTDIR"
 
--- tools/tspsd/run_plot_progress.sh
+++ tools/tspsd/run_plot_progress.sh
@@ -4,17 +4,17 @@
 CMD="python3 ./tools/tspsd/plot_progress.py"
 
 # SCP, closest, 1 min
-SOLUTIONS=("./log/scp-meta/berlin52_closest_1min/")
-OUTDIR="./figures/scp/berlin52_closest_1min_progress/"
-TYPE="SCP"
-
+#SOLUTIONS=("./log/scp-meta/berlin52_closest_1min/")
+#OUTDIR="./figures/scp/berlin52_closest_1min_progress/"
+#TYPE="SCP"
+#
 #cmake -E make_directory "$OUTDIR"
 #for SOLUTION in "$SOLUTIONS"*
 #do
 #  echo $SOLUTION
 #  $CMD -i $SOLUTION -o $OUTDIR -t $TYPE
 #done
-pdfunite $(ls -v -d $OUTDIR/*) $OUTDIR"all.pdf"
+#pdfunite $(ls -v -d $OUTDIR/*) $OUTDIR"all.pdf"
 
 
 # SCP, closest, 10 min
@@ -42,19 +42,19 @@ pdfunite $(ls -v -d $OUTDIR/*) $OUTDIR"all.pdf"
 #  $CMD -i $SOLUTION -o $OUTDIR -t $TYPE
 #done
 #pdfunite $(ls -v -d $OUTDIR/*) $OUTDIR"all.pdf"
-#
-## SCP, random, 10 min
-#SOLUTIONS=("./log/scp-meta/berlin52_random_10min/")
-#OUTDIR="./figures/scp/berlin52_random_10min_progress/"
-#TYPE="SCP"
-#
-#cmake -E make_directory "$OUTDIR"
-#for SOLUTION in "$SOLUTIONS"*
-#do
-#  echo $SOLUTION
-#  $CMD -i $SOLUTION -o $OUTDIR -t $TYPE
-#done
-#pdfunite $(ls -v -d $OUTDIR/*) $OUTDIR"all.pdf"
+
+# SCP, random, 10 min
+SOLUTIONS=("./log/scp-meta/berlin52_random_10min/")
+OUTDIR="./figures/scp/berlin52_random_10min_progress/"
+TYPE="SCP"
+
+cmake -E make_directory "$OUTDIR"
+for SOLUTION in "$SOLUTIONS"*
+do
+  echo $SOLUTION
+  $CMD -i $SOLUTION -o $OUTDIR -t $TYPE
+done
+pdfunite $(ls -v -d $OUTDIR/*) $OUTDIR"all.pdf"
 #
 #
 #
