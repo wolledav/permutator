838b4ab32ff0db8e7554baa6c4562d9c81df21bf
david <wolledav@cvut.cz>
2022-10-13 14:56:49 +0200

    fixes in generating scripts, ham. prob. drawing script added, new small dataset added

--- tests/scp.sh
+++ tests/scp.sh
@@ -6,7 +6,7 @@ DATASET_DIR="./data/tspsd/"
 LOG_DIR="./log/scp-meta/"
 
 
-D_NAME="random_tsp24"
+D_NAME="random24_v2"
 
 D_PATH="${DATASET_DIR}${D_NAME}/"
 LOG_PATH="${LOG_DIR}${D_NAME}_${TEST_TIMEOUT}s"
