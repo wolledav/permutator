94cff4e3ed82fb87e753cbb6cc63a8df41812cbe
david <wolledav@cvut.cz>
2022-10-20 16:44:50 +0200

    generating script finished

--- tools/tspsd/generate_random_ov_tspsd.py
+++ tools/tspsd/generate_random_ov_tspsd.py
@@ -4,101 +4,112 @@ import random
 import json
 import os
 import math
+import numpy as np
 
 
-def boundedIntegersWithSum(n, total, bound):
-    if n * bound < total:
-        print("Invalid input parameters!")
-        return
-    arr = [0] * n
-    added = 0
-    while added < total:
-        i = random.randint(0, n - 1)
-        if arr[i] < bound:
-            arr[i] += 1
-            added += 1
-    return arr
+def get_p_e_removed(n, k, steps):
+    if k == 0:
+        return 0
+    elif k > n - int(steps):
+        return 1
+    else:
+        return (math.comb(n, k) - math.comb(n - int(steps), k)) / math.comb(n, k)
+
+
+def get_exp_sum(edge_rem_cnt, n, steps):
+    sum = 0
+    for e in edge_rem_cnt:
+        k = edge_rem_cnt[e]
+        sum += get_p_e_removed(n, k, steps)
+    return sum
+
+
+def get_p_e_removed_v2(n, k, steps):
+    prod = 1
+    for i in range(k):
+        prod *= ((n - steps) - i)/(n - i)
+    return 1 - prod
+
+
+def get_exp_degree(edge_rem_cnt, n, steps):
+    sum = get_exp_sum(edge_rem_cnt, n, steps)
+    return (n - 1) - (2 * sum)/n
 
 
 NUM_VERTICES = 24
-PROBLEMS_PER_SIZE = 1
+PROBLEMS_PER_DEGREE = 1
+MIN_AVG_DEGREE = 0
+MAX_AVG_DEGREE = NUM_VERTICES - 1
+STEP = 0.5
 X_MAX = 100
 Y_MAX = 100
-OUTDIR = "./data/tspsd/random" + str(NUM_VERTICES) + "_ov_" + str(PROBLEMS_PER_SIZE) + "/"
-MIN_REMOVED_EDGES = 0
+EPS = 0.025
+OUTDIR = "./data/tspsd/random" + str(NUM_VERTICES) + "_ov_" + str(PROBLEMS_PER_DEGREE) + "/"
 
+# Create dataset directory
 if not os.path.exists(OUTDIR):
     os.makedirs(OUTDIR)
 
-num_edges = int(NUM_VERTICES * (NUM_VERTICES - 1) / 2)
-max_removed_edges = num_edges * NUM_VERTICES
+# Create list of all vertex->edge combinations
 all_vertices = list(range(1, NUM_VERTICES + 1))
 all_edges = []
-for node1 in all_vertices:
-    for node2 in all_vertices:
-        if node1 < node2:
-            all_edges.append([str(node1), str(node2)])
+all_deletes = []
+for e_from in all_vertices:
+    for e_to in all_vertices:
+        if e_from < e_to:
+            all_edges.append(tuple([e_from, e_to]))
+for v in all_vertices:
+    for e in all_edges:
+        all_deletes.append([v, e])
 
-print("|V| = " + str(NUM_VERTICES))
-print("|E| = " + str(num_edges))
-print("max_removed_edges = " + str(max_removed_edges))
+# Create PROBLEMS_PER_DEGREE instances for each avg degree
+for id in range(PROBLEMS_PER_DEGREE):
+    all_degrees = list(np.arange(MIN_AVG_DEGREE - STEP, MAX_AVG_DEGREE + STEP, STEP))
+    random.shuffle(all_deletes)
+    all_deletes_ = all_deletes.copy()
+    edge_removed_cnt = {}
+    coords = {}
+    delete = {}
+    groups = [0] * NUM_VERTICES
+    for e in all_edges:
+        edge_removed_cnt[e] = 0
+    for vertex in all_vertices:
+        coords[str(vertex)] = [random.randint(0, X_MAX), random.randint(0, Y_MAX)]
+        delete[str(vertex)] = []
+    total_removed_cnt = 0
+    goal_degree = all_degrees.pop()
+    while goal_degree >= MIN_AVG_DEGREE:
+        current_degree = get_exp_degree(edge_removed_cnt, NUM_VERTICES, NUM_VERTICES/2)
+        if abs(current_degree - goal_degree) < EPS: # export instance
+            print("goal_degree = " + str(goal_degree) + ", current_degree = " + str(current_degree))
+            # EXPORT ------------------------------------------
+            data = {}
+            data["NAME"] = "random-" + str(NUM_VERTICES) + "-" + str(goal_degree) + "-" + str(id)
+            data["TYPE"] = "TSPSD"
+            data["COMMENT"] = str(NUM_VERTICES) + " random locations, " + str(goal_degree) + " goal degree after n/2 deletes"
+            data["EDGE_WEIGHT_TYPE"] = "EUC_2D"
+            data["DIMENSION"] = NUM_VERTICES
+            data["GROUPS"] = groups
+            data["GROUPS_SUM"] = total_removed_cnt
+            data["EXP_DEGREE_HALF"] = current_degree
+            data["EXP_DEGREES"] = []
+            data["EXP_REMOVED_EDGES"] = []
+            for steps in range(NUM_VERTICES + 1):
+                data["EXP_DEGREES"].append(get_exp_degree(edge_removed_cnt, NUM_VERTICES, steps))
+                data["EXP_REMOVED_EDGES"].append(get_exp_sum(edge_removed_cnt, NUM_VERTICES, steps))
+            data["NODE_COORDS"] = coords
+            data["DELETE"] = delete
 
-for removed_edges_cnt in range(MIN_REMOVED_EDGES, max_removed_edges + 1):
-# for removed_edges_cnt in [10]:
-    print("Generating instances with " + str(removed_edges_cnt) + " removed edges")
-
-    for problems_cnt in range(PROBLEMS_PER_SIZE):  # generate problems_cnt problems with removed_edges_cnt edges removed
-        groups_ = boundedIntegersWithSum(NUM_VERTICES, removed_edges_cnt, num_edges)
-        groups = groups_.copy()
-        coords = {}
-        delete = {}
-        edge_removed_cnt = {}
-        for edge in all_edges:
-            edge_removed_cnt[tuple(edge)] = 0
-
-        for vertex in all_vertices:
-            coords[str(vertex)] = [random.randint(0, X_MAX), random.randint(0, Y_MAX)]  # fill coords
-            edges_cnt = groups_.pop(0)   # fill delete function
-            random.shuffle(all_edges)
-            shuffled_edges = all_edges.copy()
-            delete[str(vertex)] = []
-            for i in range(edges_cnt):
-                edge = shuffled_edges.pop(0)
-                delete[str(vertex)].append(edge)
-                edge_removed_cnt[(tuple(edge))] += 1
-
-        mean_removed_cnt = 0
-        distinct_removed_cnt = 0
-        for edge in edge_removed_cnt:
-            n = NUM_VERTICES
-            k = edge_removed_cnt[edge]
-            inc = 0
-            if k == 0:
-                inc = 0
-            elif k > math.floor(n/2):
-                inc = 1
-                distinct_removed_cnt += 1
-            else:
-                inc = (math.comb(n, k) - math.comb(math.floor(n/2), k)) / math.comb(n, k)
-                distinct_removed_cnt += 1
-            mean_removed_cnt += inc
-
-        data = {}
-        data["NAME"] = "random-" + str(NUM_VERTICES) + "-" + str(removed_edges_cnt) + "-" + str(problems_cnt)
-        data["TYPE"] = "TSPSD"
-        data["COMMENT"] = str(NUM_VERTICES) + " random locations, " + str(removed_edges_cnt) + " edges deleted in total"
-        data["GROUPS"] = groups
-        data["DISTINCT_REMOVED_HALF"] = mean_removed_cnt
-        data["DISTINCT_REMOVED"] = distinct_removed_cnt
-        data["TOTAL_REMOVED"] = removed_edges_cnt
-        data["DIMENSION"] = NUM_VERTICES
-        data["EDGE_WEIGHT_TYPE"] = "EUC_2D"
-        data["NODE_COORDS"] = coords
-        data["DELETE"] = delete
-
-
-        json_data = json.dumps(data, indent=4)
-        output_path = OUTDIR + data["NAME"] + ".json"
-        with open(output_path, "w") as outfile:
-            outfile.write(json_data)
-            print("Generated " + output_path)
+            json_data = json.dumps(data, indent=4)
+            output_path = OUTDIR + data["NAME"] + ".json"
+            with open(output_path, "w") as outfile:
+                outfile.write(json_data)
+                print("Generated " + output_path)
+            # EXPORT ------------------------------------------
+            goal_degree = all_degrees.pop()
+        else: # delete one more edge
+            e = all_deletes_.pop()
+            edge_removed_cnt[e[1]] += 1
+            total_removed_cnt += 1
+            groups[e[0] - 1] += 1
+            delete[str(e[0])].append([str(e[1][0]), str(e[1][1])])
