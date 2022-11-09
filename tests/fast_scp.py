1898b188c22b648c7617b5872d7f76a7d32ff90a
David Woller <wolledav@fel.cvut.cz>
2022-11-09 10:31:32 +0100

    test setup for solving range_estimate instances

--- tests/fast_scp.py
+++ tests/fast_scp.py
@@ -3,28 +3,23 @@
 import os
 from sortedcontainers import SortedSet
 import subprocess
-import shutil
 import json
-import time
 
-time.sleep(21600)
-
-CMD = "./cmake-build-release/scp_meta"
-TEST_TIMEOUT = 10
-DATASET_DIR = "./data/tspsd/"
+CMD = "./cmake-build-release/scp_meta_stable"
+PROBLEM_TYPE = "SCP"
+DATASET_DIR = "./data/tspsd/range_estimate/"
 LOG_DIR = "./log/scp-meta/"
 MAX_INFEASIBLE = 5
-DATASETS = ["random100_ov_100"
-            ]
+DATASETS = ["random10_ov_1", "random20_ov_1", "random30_ov_1", "random40_ov_1", "random50_ov_1", "random60_ov_1", "random70_ov_1", "random80_ov_1", "random90_ov_1", "random100_ov_1", "random200_ov_1", "random400_ov_1"]
+TEST_TIMEOUTS = [3, 3, 3, 4, 5, 6, 7, 8, 9, 10, 20, 40]
 
-for D_NAME in DATASETS:
+for D_NAME, TEST_TIMEOUT in zip(DATASETS, TEST_TIMEOUTS):
     problems_cnt = int(D_NAME.split('_')[-1])
 
     D_PATH = DATASET_DIR + D_NAME + "/"
-    LOG_PATH = LOG_DIR + D_NAME + "_" + str(TEST_TIMEOUT) + "s/"
+    LOG_PATH = LOG_DIR + D_NAME + "_" + PROBLEM_TYPE + "_" + str(TEST_TIMEOUT) + "s/"
     if not os.path.exists(LOG_PATH):
         os.makedirs(LOG_PATH)
-
 
     # Get vertex degrees and ids
     ids = SortedSet()
