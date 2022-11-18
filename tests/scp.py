300b0ec4caf41cb6a2d5d7b77ef150d9302691c1
David Woller <wolledav@fel.cvut.cz>
2022-11-18 12:31:17 +0100

    scp solving script added

--- tests/scp.py
+++ tests/scp.py
@@ -4,13 +4,13 @@ import os
 import subprocess
 
 CMD = "./cmake-build-release/scp_meta_stable"
-CONFIG = "./configs/SCP_config.json"
+CONFIG = "./configs/SCP_config_opt.json"
 PROBLEM_TYPE = "SCP"
 
 DATASET_DIR = "./data/tspsd/tsplib_10/selected/"
 LOG_DIR = "./log/scp-meta/tsplib_10/selected/"
-PROBLEMS = ["burma14-2.20"]
-TIMEOUTS = [5]
+PROBLEMS = ["burma14-2.20", "ulysses22-3.80", "berlin52-8.30", "eil101-18.00", "gr202-50.00", "lin318-70.00", "fl417-130.00", "d657-290.00"]
+TIMEOUTS = [140, 220, 520, 1010, 2020, 3180, 4170, 6570]
 CNT = 50
 
 for problem, timeout in zip(PROBLEMS, TIMEOUTS):
