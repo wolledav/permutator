ef81400ef2ea7107868b438278be1ba399fa4105
David Woller <wolledav@fel.cvut.cz>
2022-11-27 13:36:11 +0100

    timeouts set

--- tests/wcp_init.py
+++ tests/wcp_init.py
@@ -13,7 +13,7 @@ LOG_DIR = "./log/wcp-meta/tsplib_10/selected_init/"
 INIT_DIR = "./log/scp-meta/tsplib_10/BKSs/"
 
 PROBLEMS = ["burma14-2.20", "ulysses22-3.80", "berlin52-5.00", "berlin52-8.30", "eil101-18.00", "gr202-50.00", "lin318-70.00", "fl417-130.00", "d657-290.00", "rat783-460.00", "vm1084-840.00"]
-TIMEOUTS = [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+TIMEOUTS = [140, 220, 520, 520, 1010, 2020, 3180, 4170, 6570, 7830, 10840]
 
 
 SEED_FIRST = 0
