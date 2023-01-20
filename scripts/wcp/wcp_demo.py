06a4a5fbc7ae17c54d4b0bc05af807ec2e9956c6
David Woller <wolledav@fel.cvut.cz>
2023-01-20 10:15:42 +0100

    wcp demo test

--- scripts/wcp/wcp_demo.py
+++ scripts/wcp/wcp_demo.py
@@ -4,19 +4,22 @@ import os
 import subprocess
 import time
 
-CMD = "./cmake-build-release/wcp_meta_stable"
+CMD = "./cmake-build-release/wcp_meta"
 CONFIG = "./configs/SCP_config_opt_random.json"
 PROBLEM_TYPE = "WCP"
 
-DATASET_DIR = "./data/datasets/tspsd/tsplib_10/selected/"
+DATASET_DIR = "./wcp_demo/"
 LOG_DIR = "./wcp_demo/"
 
-PROBLEMS = ["burma14-2.20", "ulysses22-3.80", "berlin52-5.00", "berlin52-8.30", "eil101-18.00", "gr202-50.00", "lin318-70.00", "fl417-130.00", "d657-290.00", "rat783-460.00", "vm1084-840.00"]
-TIMEOUTS = [140, 220, 520, 520, 1010, 2020, 3180, 4170, 6570, 7830, 10840]
+# PROBLEMS = ["burma14-2.20", "ulysses22-3.80", "berlin52-5.00", "berlin52-8.30", "eil101-18.00", "gr202-50.00", "lin318-70.00", "fl417-130.00", "d657-290.00", "rat783-460.00", "vm1084-840.00"]
+# TIMEOUTS = [140, 220, 520, 520, 1010, 2020, 3180, 4170, 6570, 7830, 10840]
+
+PROBLEMS = ["fl417-160.6"]
+TIMEOUTS = [4170]
 
 
-SEED_FIRST = 0
-SEED_LAST = 5
+SEED_FIRST = 3
+SEED_LAST = 10
 
 
 for seed in range(SEED_FIRST, SEED_LAST + 1):
