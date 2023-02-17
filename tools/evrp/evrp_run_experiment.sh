fdc20c0c1e5ec9eac8442a206cf5ad47363f9c21
Jan Svrcina <svrc.jan@gmail.com>
2023-02-17 03:52:08 +0100

    final experiment setup

--- tools/evrp/evrp_run_experiment.sh
+++ tools/evrp/evrp_run_experiment.sh
@@ -1,21 +1,21 @@
 files=(
-    "evrp-E-n22-k4.json 3"
-    "evrp-E-n23-k3.json 3"
-    "evrp-E-n30-k3.json 3"
-    "evrp-E-n33-k4.json 4"
-    "evrp-E-n51-k5.json 6",
-    "evrp-E-n76-k7.json 9",
-    "evrp-E-n101-k8.json 11",
-    "evrp-X-n143-k7.json 31",
-    "evrp-X-n214-k11.json 47",
-    # "evrp-X-n351-k40.json 81",
-    # "evrp-X-n459-k26.json 101",
-    # "evrp-X-n573-k30.json 122",
-    # "evrp-X-n685-k75.json 150",
-    # "evrp-X-n749-k98.json 165",
-    # "evrp-X-n819-k171.json 179",
-    # "evrp-X-n916-k207.json 196",
-    # "evrp-X-n1001-k43.json 321"
+    "evrp-E-n22-k4.json 6"
+    "evrp-E-n23-k3.json 6"
+    "evrp-E-n30-k3.json 7"
+    "evrp-E-n33-k4.json 8"
+    "evrp-E-n51-k5.json 12",
+    "evrp-E-n76-k7.json 18",
+    "evrp-E-n101-k8.json 23",
+    "evrp-X-n143-k7.json 62",
+    "evrp-X-n214-k11.json 94",
+    "evrp-X-n351-k40.json 163",
+    "evrp-X-n459-k26.json 203",
+    "evrp-X-n573-k30.json 245",
+    "evrp-X-n685-k75.json 301",
+    "evrp-X-n749-k98.json 330",
+    "evrp-X-n819-k171.json 358",
+    "evrp-X-n916-k207.json 392",
+    "evrp-X-n1001-k43.json 643"
 )
 
 use_repair="no_repair"
@@ -35,7 +35,7 @@ do
 
     for ((seed = 1; seed <= 20; seed++))
     do
-        ./build/evrp_meta -c $config -d $data -s $seed -t $timeout >> $stats
+        ./cmake-build-release/evrp_meta -c $config -d $data -s $seed -t $timeout >> $stats
     done
 
 done
