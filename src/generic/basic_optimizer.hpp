c9bb5a91c6c136bc80436128ed70cb916534d152
pazoudav <pazoutdavid@gmail.com>
2023-03-06 17:23:55 +0100

    methods moved to basic_optimizer

--- src/generic/basic_optimizer.hpp
+++ src/generic/basic_optimizer.hpp
@@ -11,12 +11,15 @@ class BasicOptimizer
 {
     protected:
         nlohmann::json config;
+        uint timeout_s;
         Solution best_known_solution;
         std::chrono::steady_clock::time_point start, last_improvement;
         Instance* instance;
 
+        long getRuntime() {return std::chrono::duration_cast<std::chrono::seconds>(std::chrono::steady_clock::now() - this->start).count();};
+        virtual bool stop() = 0;
+
     public:
-        //BasicOptimizer() = default;
         virtual ~BasicOptimizer() {};
         virtual void run() {};
         virtual Solution getSolution() {return this->best_known_solution;};
